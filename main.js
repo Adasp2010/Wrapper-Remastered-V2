692dbac199c653624e54baf76da618e9e9e8e552
Adam The GoAnimater <155333922+Adasp2010@users.noreply.github.com>
2024-07-04 23:42:16 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,268 +1,171 @@
-const voices = require("./info").voices;
-const qs = require("querystring");
-const brotli = require("brotli");
-const https = require("https");
-const md5 = require("js-md5");
-const http = require("http");
+const exFolder = process.env.EXAMPLE_FOLDER;
+const caché = require("../asset/caché");
+const fUtil = require("../misc/file");
+const nodezip = require("node-zip");
+const parse = require("../movie/parse");
+const fs = require("fs");
+const truncate = require("truncate");
 
-// Fallback option for compatibility between Wrapper and https://github.com/Windows81/Text2Speech-Haxxor-JS.
-let get;
-try {
-	get = require("../misc/get");
-} catch {
-	get = require("./get");
-}
+module.exports = {
+	/**
+	 *
+	 * @param {Buffer} starterZip
+	 * @returns {Promise<string>}
+	 */
+	save(starterZip, thumb) {
+		return new Promise(async (res, rej) => {
+			var sId = fUtil.getNextFileId("starter-", ".xml");
+			var zip = nodezip.unzip(starterZip);
+			
+			const thumbFile = fUtil.getFileIndex("starter-", ".png", sId);
+			fs.writeFileSync(thumbFile, thumb);
+			var path = fUtil.getFileIndex("starter-", ".xml", sId);
+			var writeStream = fs.createWriteStream(path);
+			var assetBuffers = caché.loadTable(sId);
+			parse.unpackMovie(zip, thumb, assetBuffers).then((data) => {
+				writeStream.write(data, () => {
+					writeStream.close();
+					res("s-" + sId);
+				});
+			});
+				
+				
+		});
+	},
+	delete(mId) {
+		return new Promise(async (res, rej) => {
+			var i = mId.indexOf("-");
+			var prefix = mId.substr(0, i);
+			var suffix = mId.substr(i + 1);
+			switch (prefix) {
+				case "m":
+					var moviePath = fUtil.getFileIndex("movie-", ".xml", suffix);
+					var thumbPath = fUtil.getFileIndex("thumb-", ".png", suffix);
+					fs.unlinkSync(moviePath);
+					fs.unlinkSync(thumbPath);
+					caché.clearTable(mId);
+					res(mId);
+					break;
+				
+				default:
+					rej();
+			}
+		});
+	},
+	loadZip(mId) {
+		return new Promise((res, rej) => {
+			const i = mId.indexOf("-");
+			const prefix = mId.substr(0, i);
+			const suffix = mId.substr(i + 1);
+			switch (prefix) {
+				case "e": {
+					caché.clearTable(mId);
+					let data = fs.readFileSync(`${exFolder}/${suffix}.zip`);
+					res(data.subarray(data.indexOf(80)));
+					break;
+				}
+				case "m": {
+					let numId = Number.parseInt(suffix);
+					if (isNaN(numId)) res();
+					let filePath = fUtil.getFileIndex("movie-", ".xml", numId);
+					if (!fs.existsSync(filePath)) res();
 
-module.exports = (voiceName, text) => {
-	return new Promise(async (res, rej) => {
-		const voice = voices[voiceName];
-		switch (voice.source) {
-			case "nextup": {
-				https.get("https://nextup.com/ivona/index.html", (r) => {
-					var q = qs.encode({
-						voice: voice.arg,
-						language: `${voice.language}-${voice.country}`,
-						text: text,
-					});
-					var buffers = [];
-					https.get(`https://nextup.com/ivona/php/nextup-polly/CreateSpeech/CreateSpeechGet3.php?${q}`, (r) => {
-						r.on("data", (d) => buffers.push(d));
-						r.on("end", () => {
-							const loc = Buffer.concat(buffers).toString();
-							if (!loc.startsWith("http")) rej();
-							get(loc).then(res).catch(rej);
+					const buffer = fs.readFileSync(filePath);
+					if (!buffer || buffer.length == 0) res();
+
+					try {
+						parse.packMovie(buffer, mId).then((pack) => {
+						parse.packXml(buffer, mId).then(v => res(v));
+							caché.saveTable(mId, pack.caché);
+							res(pack.zipBuf);
 						});
-						r.on("error", rej);
-					});
-				});
-				break;
-			}
-			case "polly": {
-				var buffers = [];
-				var req = https.request(
-					{
-						hostname: "pollyvoices.com",
-						port: "443",
-						path: "/api/sound/add",
-						method: "POST",
-						headers: {
-							"Content-Type": "application/x-www-form-urlencoded",
-						},
-					},
-					(r) => {
-						r.on("data", (b) => buffers.push(b));
-						r.on("end", () => {
-							var json = JSON.parse(Buffer.concat(buffers));
-							if (json.file) get(`https://pollyvoices.com${json.file}`).then(res);
-							else rej();
-						});
+						break;
+					} catch (e) {
+						res();
 					}
-				);
-				req.write(qs.encode({ text: text, voice: voice.arg }));
-				req.end();
-				break;
+				}
+				default:
+					res();
 			}
-			case "cepstral":
-			case "voiceforge": {
-				https.get("https://www.voiceforge.com/demo", (r) => {
-					const cookie = r.headers["set-cookie"];
-					var q = qs.encode({
-						voice: voice.arg,
-						voiceText: text,
-					});
-					var buffers = [];
-					https.get(
-						{
-							host: "www.voiceforge.com",
-							path: `/demos/createAudio.php?${q}`,
-							headers: { Cookie: cookie },
-						},
-						(r) => {
-							r.on("data", (b) => buffers.push(b));
-							r.on("end", () => {
-								const html = Buffer.concat(buffers);
-								const beg = html.indexOf('id="mp3Source" src="') + 20;
-								const end = html.indexOf('"', beg);
-								const loc = html.subarray(beg, end).toString();
-								get(`https://www.voiceforge.com${loc}`).then(res).catch(rej);
-							});
-						}
-					);
-				});
-				break;
+		});
+	},
+	loadXml(movieId) {
+		return new Promise(async (res, rej) => {
+			const i = movieId.indexOf("-");
+			const prefix = movieId.substr(0, i);
+			const suffix = movieId.substr(i + 1);
+			switch (prefix) {
+				case "m": {
+					const fn = fUtil.getFileIndex("movie-", ".xml", suffix);
+					if (fs.existsSync(fn)) res(fs.readFileSync(fn));
+					else rej();
+					break;
+				}
+				case "e": {
+					const fn = `${exFolder}/${suffix}.zip`;
+					if (!fs.existsSync(fn)) return rej();
+					parse
+						.unpackMovie(nodezip.unzip(fn))
+						.then((v) => res(v))
+						.catch((e) => rej(e));
+					break;
+				}
+				default:
+					rej();
 			}
-			case "vocalware": {
-				var [eid, lid, vid] = voice.arg;
-				var cs = md5(`${eid}${lid}${vid}${text}1mp35883747uetivb9tb8108wfj`);
-				var q = qs.encode({
-					EID: voice.arg[0],
-					LID: voice.arg[1],
-					VID: voice.arg[2],
-					TXT: text,
-					EXT: "mp3",
-					IS_UTF8: 1,
-					ACC: 5883747,
-					cache_flag: 3,
-					CS: cs,
-				});
-				var req = https.get(
-					{
-						host: "cache-a.oddcast.com",
-						path: `/tts/gen.php?${q}`,
-						headers: {
-							Referer: "https://www.oddcast.com/",
-							Origin: "https://www.oddcast.com/",
-							"User-Agent":
-								"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36",
-						},
-					},
-					(r) => {
-						var buffers = [];
-						r.on("data", (d) => buffers.push(d));
-						r.on("end", () => res(Buffer.concat(buffers)));
-						r.on("error", rej);
-					}
-				);
-				break;
-			}
-			case "voicery": {
-				var q = qs.encode({
-					text: text,
-					speaker: voice.arg,
-					ssml: text.includes("<"),
-					//style: 'default',
-				});
-				https.get(
-					{
-						host: "www.voicery.com",
-						path: `/api/generate?${q}`,
-					},
-					(r) => {
-						var buffers = [];
-						r.on("data", (d) => buffers.push(d));
-						r.on("end", () => res(Buffer.concat(buffers)));
-						r.on("error", rej);
-					}
-				);
-				break;
-			}
-			case "watson": {
-				var q = qs.encode({
-					text: text,
-					voice: voice.arg,
-					download: true,
-					accept: "audio/mp3",
-				});
-				https.get(
-					{
-						host: "text-to-speech-demo.ng.bluemix.net",
-						path: `/api/v1/synthesize?${q}`,
-					},
-					(r) => {
-						var buffers = [];
-						r.on("data", (d) => buffers.push(d));
-						r.on("end", () => res(Buffer.concat(buffers)));
-						r.on("error", rej);
-					}
-				);
-				break;
-			}
-			case "acapela": {
-				var q = qs.encode({
-					cl_login: "VAAS_MKT",
-					req_snd_type: "",
-					req_voice: voice.arg,
-					cl_app: "seriousbusiness",
-					req_text: text,
-					cl_pwd: "M5Awq9xu",
-				});
-				http.get(
-					{
-						host: "vaassl3.acapela-group.com",
-						path: `/Services/AcapelaTV/Synthesizer?${q}`,
-					},
-					(r) => {
-						var buffers = [];
-						r.on("data", (d) => buffers.push(d));
-						r.on("end", () => {
-							const html = Buffer.concat(buffers);
-							const beg = html.indexOf("&snd_url=") + 9;
-							const end = html.indexOf("&", beg);
-							const sub = html.subarray(beg + 4, end).toString();
-							if (!sub.startsWith("://")) return rej();
-							get(`https${sub}`).then(res).catch(rej);
-						});
-						r.on("error", rej);
-					}
-				);
-				break;
-			}
-			case "readloud": {
-				const req = https.request(
-					{
-						host: "readloud.net",
-						path: voice.arg,
-						method: "POST",
-						port: "443",
-						headers: {
-							"Content-Type": "application/x-www-form-urlencoded",
-						},
-					},
-					(r) => {
-						var buffers = [];
-						r.on("data", (d) => buffers.push(d));
-						r.on("end", () => {
-							const html = Buffer.concat(buffers);
-							const beg = html.indexOf("/tmp/");
-							const end = html.indexOf(".mp3", beg) + 4;
-							const sub = html.subarray(beg, end).toString();
-							const loc = `https://readloud.net${sub}`;
-							get(loc).then(res).catch(rej);
-						});
-						r.on("error", rej);
-					}
-				);
-				req.end(
-					qs.encode({
-						but1: text,
-						but: "Enviar",
-					})
-				);
-				break;
-			}
-			case "cereproc": {
-				const req = https.request(
-					{
-						hostname: "www.cereproc.com",
-						path: "/themes/benchpress/livedemo.php",
-						method: "POST",
-						headers: {
-							"content-type": "text/xml",
-							"accept-encoding": "gzip, deflate, br",
-							origin: "https://www.cereproc.com",
-							referer: "https://www.cereproc.com/en/products/voices",
-							"x-requested-with": "XMLHttpRequest",
-							cookie: "Drupal.visitor.liveDemo=666",
-						},
-					},
-					(r) => {
-						var buffers = [];
-						r.on("data", (d) => buffers.push(d));
-						r.on("end", () => {
-							const xml = String.fromCharCode.apply(null, brotli.decompress(Buffer.concat(buffers)));
-							const beg = xml.indexOf("https://cerevoice.s3.amazonaws.com/");
-							const end = xml.indexOf(".mp3", beg) + 4;
-							const loc = xml.substr(beg, end - beg).toString();
-							get(loc).then(res).catch(rej);
-						});
-						r.on("error", rej);
-					}
-				);
-				req.end(
-					`<speakExtended key='666'><voice>${voice.arg}</voice><text>${text}</text><audioFormat>mp3</audioFormat></speakExtended>`
-				);
-				break;
-			}
+		});
+	},
+	thumb(movieId) {
+		return new Promise(async (res, rej) => {
+			if (!movieId.startsWith("s-")) return;
+			const n = Number.parseInt(movieId.substr(2));
+			const fn = fUtil.getFileIndex("starter-", ".png", n);
+			isNaN(n) ? rej() : res(fs.readFileSync(fn));
+		});
+	},
+	list() {
+		const table = [];
+		var ids = fUtil.getValidFileIndicies("starter-", ".xml");
+		for (const i in ids) {
+			var id = `s-${ids[i]}`;
+			table.unshift({ id: id });
 		}
-	});
+		return table;
+	},
+	meta(movieId) {
+		return new Promise(async (res, rej) => {
+			if (!movieId.startsWith("m-")) return;
+			const n = Number.parseInt(movieId.substr(2));
+			const fn = fUtil.getFileIndex("movie-", ".xml", n);
+
+			const fd = fs.openSync(fn, "r");
+			const buffer = Buffer.alloc(256);
+			fs.readSync(fd, buffer, 0, 256, 0);
+			const begTitle = buffer.indexOf("<title>") + 16;
+			const endTitle = buffer.indexOf("]]></title>");
+			const title = buffer.slice(begTitle, endTitle).toString().trim();
+
+			const begDesc = buffer.indexOf("<desc>") + 15;
+			const endDesc = buffer.indexOf("]]></desc>");
+			const longDesc = buffer.slice(begDesc, endDesc).toString().trim();
+			const desc = truncate(longDesc, 51);
+
+			const begDuration = buffer.indexOf('duration="') + 10;
+			const endDuration = buffer.indexOf('"', begDuration);
+			const duration = Number.parseFloat(buffer.slice(begDuration, endDuration));
+			const min = ("" + ~~(duration / 60)).padStart(2, "0");
+			const sec = ("" + ~~(duration % 60)).padStart(2, "0");
+			const durationStr = `${min}:${sec}`;
+
+			fs.closeSync(fd);
+			res({
+				date: fs.statSync(fn).mtime,
+				durationString: durationStr,
+				duration: duration,
+				title: title,
+				desc: desc,
+				id: movieId,
+			});
+		});
+	},
 };
